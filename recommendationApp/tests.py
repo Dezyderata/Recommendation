c4045c1e373c32cb4afa0892ff369460dc579dc9
agniesia <s15038@pjwstk.edu.pl>
2021-07-02 13:54:38 +0200

    Simple tests

--- recommendationApp/tests.py
+++ recommendationApp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from recommendationApp.models import Movie, User, Rating
 # Create your tests here.
+
+
+class MovieTestCase(TestCase):
+
+    def test_movie_str(self):
+        first_movie = Movie.objects.create(movie_name="Scary Movie")
+        self.assertEqual(first_movie.__str__(), "Scary Movie")
+
+class UserTestCase(TestCase):
+
+    def test_user_str(self):
+        first_user = User.objects.create(user_name="Jose")
+        self.assertEqual(first_user.__str__(), "Jose")
