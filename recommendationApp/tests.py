1e90b9c072e47bba78bb712cebcbef63858939da
agniesia <s15038@pjwstk.edu.pl>
2021-07-04 15:14:19 +0200

    new migration tests

--- recommendationApp/tests.py
+++ recommendationApp/tests.py
@@ -9,8 +9,9 @@ class MovieTestCase(TestCase):
         first_movie = Movie.objects.create(movie_name="Scary Movie")
         self.assertEqual(first_movie.__str__(), "Scary Movie")
 
+
 class UserTestCase(TestCase):
 
     def test_user_str(self):
-        first_user = User.objects.create(user_name="Jose")
+        first_user = User.objects.create(username="Jose")
         self.assertEqual(first_user.__str__(), "Jose")
